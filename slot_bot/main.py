9ca9617510f466d3e38227ffbce7ceee5a54e636
k2kouyamana4-stack <k2.kouya.mana4@gmail.com>
2026-03-29 01:41:58 +0900

    s

--- slot_bot/main.py
+++ slot_bot/main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import discord
 from discord import app_commands
 from discord.ext import commands
