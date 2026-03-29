c2153cdc2ba8f3a6f7e4921c8589185d6bdd9179
k2kouyamana4-stack <k2.kouya.mana4@gmail.com>
2026-03-30 02:40:55 +0900

    ssss

--- slot_bot/main.py
+++ slot_bot/main.py
@@ -9,7 +9,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import discord
 from discord.ext import commands
 
-from db import (
+from shared.db import (
     get_money,
     add_money,
     get_setting,
